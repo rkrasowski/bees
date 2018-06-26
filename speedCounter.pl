4dc0cd02edc5895911095485c9aa15cffe22b84e
Robert J. Krasowski <robert@krasowski.net>
2018-06-26 02:24:32 +0000

    Calculate time between sensors, coun bees IN OUT, only for one channel

---
+++ speedCounter.pl
@@ -0,0 +1,153 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Time::HiRes  qw(tv_interval gettimeofday);
+
+
+
+my $data = "11100101110101111010100111010101\n";
+print "Data: $data\n";
+				
+##########################  Config parameters #########################
+my $in = 0;
+my $out = 0;
+my $numOfChannels = 12;
+my $delay = 11111; 		#delay ms
+#######################################################################
+
+# Create starting time array 
+
+my @timeArray;
+my $timeArray;
+
+for (my $j=1; $j <= $numOfChannels; $j++)
+	{
+		
+		my $timeMs = `date +%s%N | cut -b1-13`;		#time in ms
+		push @timeArray, $timeMs;
+	}
+
+
+
+# Getting data
+
+my @inArray= (0,0,0,0,0,0,0,0,0,0,0,0);
+my $inArray;
+my @outArray= (0,0,0,0,0,0,0,0,0,0,0,0);
+my $outArray;
+my @timeBeeStart = (0,0,0,0,0,0,0,0,0,0,0,0);
+my $timeBeeStart;
+my $timeBeeFinish;
+my $timeBeeDiff;
+my @timeBeeDBIn;
+my $timeBeeDBIn;
+my @timeBeeDBOut;
+my $timeBeeDBOut;
+
+my  $start = [gettimeofday];
+
+my @dataArray = split (//,$data);
+my $dataArray;
+
+# IN triggered:
+
+if ($dataArray[0] == 1 and $dataArray[1] == 0)
+	{
+		if ($inArray[0] == 0)    					# Checking if channel IN is ative		
+			{
+				$timeBeeStart[0] =	`date +%s%N | cut -b1-13`;
+				$in = $in + 1;
+				$inArray[0] = 1;				# Desactivating IN channel
+			}
+	}
+
+
+# IN and OUT triggered direction OUT	
+
+if ($dataArray[0] == 1 and $dataArray[1] == 1 and $inArray[0] == 1)
+	{
+
+		if ($outArray[0] == 0)						# Checking if channel OUT is active
+			{
+				$timeBeeFinish = `date +%s%N | cut -b1-13`;
+				$timeBeeDiff = $timeBeeFinish - $timeBeeStart[0];
+				push (@timeBeeDBOut,$timeBeeDiff);
+				$outArray[0] = 1;
+			}
+	}	
+
+# OUT triggered
+
+if ($dataArray[1] == 1 and $dataArray[0] == 0) 
+	{
+		if ($outArray[0] == 0)    					# Checking if channel OUT is ative		
+			{
+				$timeBeeStart[0] =	`date +%s%N | cut -b1-13`;
+				$out = $out + 1;
+				$outArray[0] = 1;				# Desactivating OUT channel
+			}
+
+
+# OUT and IN triggered direction IN 
+			
+		if ($inArray[0] == 0  and $dataArray[0] == 1 and $outArray[1] == 1)			# Checking if channel IN is active
+			{
+				$timeBeeFinish = `date +%s%N | cut -b1-13`;
+				$timeBeeDiff = $timeBeeFinish - $timeBeeStart[0];
+				push (@timeBeeDBIn,$timeBeeDiff);
+				$inArray[0] = 1;
+			}
+	}	
+
+
+
+# Reactivate channel 
+
+if ($dataArray[0] == 0  and $dataArray[1] ==0)
+	{
+		$inArray[0] = 0;
+		$outArray[0] = 0;
+	}
+
+
+
+
+
+
+foreach $inArray (@inArray)
+	{
+		print "InArray: $inArray\n";
+	}
+
+foreach $outArray(@outArray)
+	{
+		print "OutArray: $outArray\n";
+
+	}
+
+foreach $timeBeeDBIn(@timeBeeDBIn)
+	{
+		print "TimeBee IN: $timeBeeDBIn\n";
+	}
+
+
+foreach $timeBeeDBOut (@timeBeeDBOut)
+	{
+		print "TimeBee OUT: $timeBeeDBOut\n";
+	}
+#		for (my $i=1; $i <= $numOfChannels; $i++) 
+#			{
+   
+			
+#			}
+
+
+my $finish = [gettimeofday];
+my $elapsed = tv_interval($start,$finish);
+
+
+
+print "Data IN: $in, OUT: $out\n";
+print "Elapsed Time: $elapsed\n";
+
+############################################## Subroutines #########################################################
